eb993e5e6c6624507882de4840ac673d27a36674
Murilo <muriloue@gmail.com>
2022-02-26 15:03:16 -0300

    Netlify update.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
 import { Container, FormWrapper, Section, Title, Form, Error, InputWrapper, InputArea, Input, Span, Button, OptionsWrapper, Option } from "./styles";
 
 const Login: React.FC = () => {
+
+    function handleSubmit(e: React.MouseEvent) {
+        e.preventDefault()
+    }
+
     return (
         <Section>
             <Container>
                 <FormWrapper>
                     <Title>LOGIN</Title>
-                    {/* <Error>Mensagem de erro</Error> */}
+                    <Error>Mensagem de erro</Error>
                     <Form>
                         <InputWrapper>
                             <Span>Email</Span>
@@ -20,11 +26,13 @@ const Login: React.FC = () => {
                                 <Input type="password"/>
                             </InputArea>
                         </InputWrapper>
-                        <Button>Entrar</Button>
+                        <Button onClick={(e) => handleSubmit(e)}>Entrar</Button>
                     </Form>
                     <OptionsWrapper>
                         <Option>Esqueceu a senha?</Option>
-                        <Option>Registrar-se</Option>
+                        <Link to={"/register"}>
+                            <Option>Registrar-se</Option>
+                        </Link>
                     </OptionsWrapper>
                 </FormWrapper>
             </Container>
